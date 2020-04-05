5a37fea52b086fefbd5ba6ab6bdcafba7cabeef7
Chan Nyein Thaw <channyeinthaw@amdon.com>
2020-04-05 15:22:22 +0630

    add docs

---
+++ docs/precache-manifest.4bf60d4b24cd78d98bcb0d2a28eef9ba.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "002a33a1d8a896b382040c762d61fd0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3896885115b36db3af1e",
+    "url": "/static/css/main.02ef870f.chunk.css"
+  },
+  {
+    "revision": "c2037b0809b48b627ac1",
+    "url": "/static/js/2.f46537d5.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/static/js/2.f46537d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3896885115b36db3af1e",
+    "url": "/static/js/main.94cf8ce6.chunk.js"
+  },
+  {
+    "revision": "96b4704aaa8748c849f8",
+    "url": "/static/js/runtime-main.2c5c9f5e.js"
+  },
+  {
+    "revision": "d12b386fa6c11c8ea7b2bda5eaa9b9a1",
+    "url": "/static/media/arrow.d12b386f.svg"
+  }
+]);
