10ebc410f21ab9f030ab53128603cc6cd3c0686d
Chan Nyein Thaw <channyeinthaw@amdon.com>
2020-04-19 22:22:04 +0630

    update letter query

--- functions/letters/letters.js
+++ functions/letters/letters.js
@@ -1,7 +1,9 @@
 async function getLetters(collection, limit, skip) {
     const counter = await collection.get()
     const documents = await (
-        collection.orderBy('createdAt', 'desc')
+        collection
+            .where('createdAt', '<', new Date().getTime())
+            .orderBy('createdAt', 'desc')
             .offset(+skip)
             .limit(+limit).get()
     )
